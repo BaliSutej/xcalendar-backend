adc7ba300be98ea749c745baf7f8638969f24e7a
sutej bali <sutejbali@gmail.com>
2022-01-27 16:53:20 +0530

    Added endpoints

--- src/app.js
+++ src/app.js
@@ -11,6 +11,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.get("/xcalendar/calendar/:calendarid",(req,res,next) => {
+    console.log(req.params.calendarid);
+    res.send("ok");
+});
+
+app.post("/xcalendar/calendar/addcalendar",(req,res,next) => {
+    console.log("Add calendar");
+    console.log(JSON.stringify(req.body));
+    res.send("ok");
+});
+
+app.get("/xcalendar/events/",(req,res,next) => {
+    console.log("All events");
+    res.send("ok");
+});
+
+
+app.post("/xcalendar/events/add",(req,res,next) => {
+    console.log("Add event");
+    console.log(JSON.stringify(req.body));
+    res.send("ok");
+});
+
+
+app.post("/xcalendar/user/login",(req,res,next) => {
+    console.log("Login");
+    console.log(JSON.stringify(req.body));
+    res.send("ok");
+});
 
 app.use((req, res, next) => {
     res.statusCode = 404;
